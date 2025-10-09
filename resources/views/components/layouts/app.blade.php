7bbb8785b475bd6df0b66124e7f29a008dbbf53b
Bumpyy <fsghassan2429d@gmail.com>
2025-10-09 07:38:02 +0700

    Update background color and adjust layout for improved visual consistency

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,7 +5,7 @@
     @include('partials.head')
 </head>
 
-<body class="font-poppins bg-cedea-red antialiased">
+<body class="font-poppins bg-[#e91f24] antialiased">
 
     <div class="bg-radial to-cedea-dark from-30 h-full min-h-dvh from-transparent">
         <div class="asia-pattern-body min-h-dvh bg-no-repeat">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
         {{-- hero --}}
         <div class="container relative mx-auto my-24 flex flex-col items-center justify-center gap-16 px-6 lg:my-32">
             <div
-                class="bg-size-[200%_200%] relative z-10 -mb-20 w-[20%] overflow-clip rounded-full bg-[url('../assets/patterns/paper.png')] bg-no-repeat drop-shadow-md">
+                class="bg-size-[200%_200%] relative z-10 -mb-[4.5rem] w-[40%] overflow-clip rounded-full bg-[url('../assets/patterns/paper.png')] bg-no-repeat drop-shadow-md lg:-mb-20 lg:w-[20%]">
                 <div
-                    class="bg-radial to-cedea-dark/10 from-30 from-transparent px-[clamp(1.5rem,_1vw,_4rem)] py-[clamp(0.7rem,_1vw,_2rem)]">
+                    class="bg-radial to-cedea-dark/10 from-30 from-transparent px-[clamp(0.8rem,_1vw,_4rem)] py-[clamp(0.4rem,_1vw,_2rem)]">
                     <img src="{{ asset('img/brand.png') }}" srcset="{{ asset('img/brand.svg') }}" alt="Cedea k-food logo">
                 </div>
             </div>
@@ -32,40 +32,40 @@
             </div>
         </div>
 
-        <div class="container relative mx-auto">
+        <div class="container relative mx-auto max-lg:pt-16">
             {{-- sticker --}}
             <div
-                class="bg-size-[100%_100%] text-cedea-red font-bebas rounded-4xl top-2/5 absolute -left-[2%] z-[1] -translate-y-1/2 -rotate-6 bg-[url('../assets/patterns/paper.png')] bg-no-repeat px-6 py-8 text-center text-[clamp(1.5rem,_4vw,_3rem)] font-bold uppercase italic leading-none">
+                class="bg-size-[100%_100%] text-cedea-red font-bebas rounded-4xl lg:top-2/5 absolute left-12 top-0 z-[1] -rotate-6 bg-[url('../assets/patterns/paper.png')] bg-no-repeat px-6 py-8 text-center text-[clamp(1.5rem,_4vw,_3rem)] font-bold uppercase italic leading-none lg:-left-[2%] lg:-translate-y-1/2">
                 menangkan
                 <br />
                 hadiahnya
             </div>
             <div
-                class="bg-size-[100%_100%] relative flex items-center justify-center bg-no-repeat py-16 max-lg:flex-col lg:min-h-[5rem] lg:bg-[url('../assets/patterns/prize-bg-transparent.png')] lg:px-64">
+                class="bg-size-[100%_100%] relative flex items-center justify-center bg-no-repeat py-16 drop-shadow-xl max-lg:flex-col lg:min-h-[5rem] lg:bg-[url('../assets/patterns/prize-bg-transparent.png')] lg:px-64">
 
                 <div class="relative">
                     <div class="relative z-10 flex flex-col items-start">
                         <p class="font-marimpa text-[clamp(1.5rem,_15vw,_30rem)] leading-none text-amber-300">10</p>
                         <p class="font-montserrat -mt-4 flex flex-col pl-[clamp(1rem,_15vw,_2rem)] text-white">
-                            <span class="text-[clamp(1rem,_2.5vw,_10rem)] font-bold uppercase leading-none">tiket</span>
-                            <span class="text-[clamp(1rem,_2.5vw,_10rem)] font-bold uppercase leading-none">ke
+                            <span class="text-[clamp(1rem,_2.5vw,_3rem)] font-bold uppercase leading-none">tiket</span>
+                            <span class="text-[clamp(1rem,_2.5vw,_3rem)] font-bold uppercase leading-none">ke
                                 seoul</span>
-                            <span class="text-[clamp(1rem,_1.5vw,_10rem)] uppercase leading-none">2 tiket untuk 5
+                            <span class="text-[clamp(1rem,_1.5vw,_2rem)] uppercase leading-none">2 tiket untuk 5
                                 pemenang</span>
                         </p>
                     </div>
-                    <img class="absolute right-0 top-2 z-0 lg:-right-40 lg:top-12 lg:-ml-28"
+                    <img class="xl:top-[clamp(6rem,_1vw,_6rem) xl:top-[clamp(6rem,_1vw,_6rem) ] right-0 top-2 z-0 max-lg:max-w-80 lg:absolute lg:-right-[clamp(10rem,_1vw,_-11rem)] lg:-ml-28 xl:-right-[clamp(10rem,_1vw,_-11rem)]"
                         src="{{ asset('img/plane.png') }}" alt="">
                 </div>
 
                 <div class="lg:ml-40">
                     <img src="{{ asset('img/money.png') }}" alt="">
-                    <div class="flex items-start">
+                    <div class="flex items-start gap-2">
                         <p class="font-marimpa text-[clamp(1.5rem,_5vw,_10rem)] leading-none text-amber-300">100</p>
                         <p class="font-montserrat flex flex-col text-white">
-                            <span class="font-bold uppercase">Juta</span>
-                            <span class="font-bold uppercase">Rupiah</span>
-                            <span>untuk 10 pemenang</span>
+                            <span class="text-[clamp(1rem,_2.5vw,_2rem)] font-bold uppercase leading-none">Juta</span>
+                            <span class="text-[clamp(1rem,_2.5vw,_2rem)] font-bold uppercase leading-none">Rupiah</span>
+                            <span class="text-[clamp(1rem,_2.5vw,_1rem)] leading-none">untuk 10 pemenang</span>
                         </p>
                     </div>
                 </div>
