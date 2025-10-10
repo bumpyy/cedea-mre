8d7733ccbb4bf32895c7f01bd00a32bc750b26fb
Bumpyy <fsghassan2429d@gmail.com>
2025-10-11 03:27:57 +0700

    rework how section

--- resources/views/components/home/how.blade.php
+++ resources/views/components/home/how.blade.php
@@ -1,50 +1,52 @@
-<div class="clamp-[my,8,16] flex w-full flex-col gap-y-3 max-lg:order-1">
-    <div
-        class="rounded-4xl clamp-[px,4,8] clamp-[py,4,12] clamp-[pt,12,24] relative grid items-center justify-center gap-x-12 gap-y-12 bg-white drop-shadow-2xl md:grid-cols-3 lg:grid-rows-[1fr_auto]">
-
+<div class="max-lg:z-[1] max-lg:order-1">
+    <div class="clamp-[my,8,16] flex w-full flex-col gap-y-3 lg:w-[120%]">
         <div
-            class="bg-cedea-gold font-bebas clamp-[text,3xl,7xl] clamp-[px,4,8] clamp-[py,2,4] absolute -top-[clamp(8%,20vw,15%)] left-1/2 w-max -translate-x-1/2 translate-y-full rounded-xl font-bold uppercase italic text-white lg:-top-[35%]">
-            <p>Cara ikutan</p>
+            class="rounded-4xl clamp-[px,4,8] clamp-[pb,4,6] clamp-[pt,12,24] relative grid items-center justify-center gap-x-12 gap-y-12 bg-white drop-shadow-2xl md:grid-cols-3 lg:grid-rows-[1fr_auto]">
+
+            <div
+                class="bg-cedea-gold font-bebas clamp-[text,3xl,7xl] clamp-[px,4,8] clamp-[py,2,4] absolute -top-[clamp(8%,20vw,15%)] left-1/2 w-max -translate-x-1/2 translate-y-full rounded-xl font-bold uppercase italic text-white lg:-top-[35%]">
+                <p>Cara ikutan</p>
+            </div>
+
+            @php
+                $how_item = [
+                    [
+                        'image' => 'img/qr.png',
+                        'label' => 'Scan',
+                        'desc' => 'QR Code di kemasan',
+                    ],
+                    [
+                        'image' => 'img/pc.png',
+                        'label' => 'Registration',
+                        'desc' => 'Melalui website',
+                    ],
+                    [
+                        'image' => 'img/receipt.png',
+                        'label' => 'upload',
+                        'desc' => 'Struk asli / foto bukti pembelian',
+                    ],
+                ];
+            @endphp
+
+            @foreach ($how_item as $item)
+                <div class="row-span-2 grid md:grid-rows-subgrid">
+                    <div class="flex items-center justify-center">
+                        <img src="{{ asset($item['image']) }}" alt="">
+                    </div>
+
+                    <div class='text-cedea-gold font-bebas flex flex-col items-center justify-start pt-2 text-center'>
+                        <p class="text-4xl uppercase">{{ $item['label'] }}</p>
+                        <p>{{ $item['desc'] }}</p>
+                    </div>
+                </div>
+            @endforeach
         </div>
 
-        @php
-            $how_item = [
-                [
-                    'image' => 'img/qr.png',
-                    'label' => 'Scan',
-                    'desc' => 'QR Code di kemasan',
-                ],
-                [
-                    'image' => 'img/pc.png',
-                    'label' => 'Registration',
-                    'desc' => 'Melalui website',
-                ],
-                [
-                    'image' => 'img/receipt.png',
-                    'label' => 'upload',
-                    'desc' => 'Struk asli / foto bukti pembelian',
-                ],
-            ];
-        @endphp
-
-        @foreach ($how_item as $item)
-            <div class="row-span-2 grid md:grid-rows-subgrid">
-                <div class="flex items-center justify-center">
-                    <img src="{{ asset($item['image']) }}" alt="">
-                </div>
-
-                <div class='text-cedea-gold font-bebas flex flex-col items-center justify-start pt-2 text-center'>
-                    <p class="text-4xl uppercase">{{ $item['label'] }}</p>
-                    <p>{{ $item['desc'] }}</p>
-                </div>
-            </div>
-        @endforeach
+        <a class="text-center uppercase" href="#">
+            <p class="clamp-[text,lg,xl] inline-flex items-center justify-center font-bold text-white">
+                <span>Selengkapnya</span>
+                <x-lucide-arrow-right class="size-6" />
+            </p>
+        </a>
     </div>
-
-    <a class="text-center uppercase" href="#">
-        <p class="clamp-[text,lg,xl] inline-flex items-center justify-center font-bold text-white">
-            <span>Selengkapnya</span>
-            <x-lucide-arrow-right class="size-6" />
-        </p>
-    </a>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,11 @@
 
         <x-home.prize />
 
-        <div class="container flex items-center justify-center gap-4 max-lg:flex-col">
+        <div class="container grid items-center justify-center gap-4 lg:grid-cols-2">
             <x-home.how />
 
-            <div class="max-lg:order-0 max-lg:clamp-[mb,-44,-26rem]">
-                <img src="{{ asset('img/zee.png') }}" alt="">
+            <div class="max-lg:order-0 max-lg:clamp-[mb,-44,-26rem] relative z-0 lg:-ml-12 lg:w-[130%]">
+                <img class="" src="{{ asset('img/zee.png') }}" alt="">
             </div>
         </div>
     </div>
