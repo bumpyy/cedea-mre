e8203a81afbb0f1b3cce9ca8f421e99136b51f0f
Bumpyy <fsghassan2429d@gmail.com>
2025-10-11 13:47:50 +0700

    fix(user): :art: fix dashboard height problem

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,20 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html class="h-full min-h-dvh" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     @include('partials.head')
 </head>
 
-<body class="font-poppins bg-[#e51b1f] antialiased">
-
-    <div class="bg-radial from-60 relative z-0 h-full min-h-dvh from-transparent to-[#952227]">
-        <div class="asia-pattern-body relative z-0 min-h-dvh overflow-clip bg-no-repeat">
+<body class="font-poppins relative h-full bg-[#e51b1f] antialiased">
+    <div class="bg-radial from-60 auto z-0 h-full from-transparent to-[#952227]">
+        <div class="asia-pattern-body z-0 flex h-full flex-col overflow-x-clip bg-no-repeat">
             <x-header />
             {{ $slot }}
             <x-footer />
         </div>
     </div>
-
     @fluxScripts
 </body>
 
