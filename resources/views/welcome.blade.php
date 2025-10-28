6ad2a7fe95d3721b7fd3a0f234252bec7a6d3b45
Ghassan Fs <fsghassan2429d@gmail.com>
2025-10-28 15:47:10 +0700

    improve mobile layout & animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,29 +6,29 @@
         </div>
 
         <div class="relative flex max-md:flex-col">
-            <div class="flex flex-col items-center justify-start gap-4">
+            <div class="flex flex-col items-center justify-start gap-4 max-md:z-[1]">
                 <div class="w-3/5">
                     <img src="{{ asset('img/eomuk-bar-text.png') }}" alt="snack ala korea Eomuk Bar">
                 </div>
-                <div class="w-11/12">
+                <div class="w-1/2 md:w-11/12">
                     <img src="{{ asset('img/enak-bar-bar.png') }}" alt="Enaknya Bar Bar">
                 </div>
             </div>
 
-            <div class="group -mt-12 w-full basis-3/5 max-md:absolute max-md:-z-10 md:-ml-12 md:-mt-24 md:flex">
+            <div class="group -mt-12 w-full basis-3/5 max-md:absolute max-md:-z-0 md:-ml-12 md:-mt-24 md:flex">
                 <div
-                    class="w-2/5 -rotate-3 drop-shadow-2xl transition-[rotate] duration-[600ms] ease-in-out group-hover:-rotate-1 max-md:absolute max-md:-left-16 md:ml-20">
+                    class="w-2/5 -rotate-3 drop-shadow-2xl transition-[rotate,scale] duration-[600ms] ease-in-out hover:z-[1] hover:scale-105 group-hover:-rotate-1 max-md:absolute max-md:-left-16 md:ml-20">
                     <img src="{{ asset('img/eomuk-cheese-4.png') }}" alt="">
                 </div>
                 <div
-                    class="w-2/5 rotate-6 drop-shadow-2xl transition-[rotate] duration-[600ms] ease-in-out group-hover:rotate-3 max-md:absolute max-md:-right-16 md:-ml-[5.3rem] md:mt-16">
+                    class="w-2/5 rotate-6 drop-shadow-2xl transition-[rotate,scale] duration-[600ms] ease-in-out hover:z-[1] hover:scale-105 group-hover:rotate-3 max-md:absolute max-md:-right-16 md:-ml-[5.3rem] md:mt-16">
                     <img src="{{ asset('img/eomuk-chili-4.png') }}" alt="">
                 </div>
                 {{-- <img src="{{ asset('img/eomuk-combine.png') }}" alt="Eomuk Combine Image"> --}}
             </div>
         </div>
 
-        <div class="-mt-4 2xl:-mt-40">
+        <div class="mt-2 2xl:-mt-40">
 
             <div class="-mb-32 md:-mb-72 md:w-2/5">
                 <img src="{{ asset('img/zee-how.png') }}" alt="snack ala korea Eomuk Bar">
