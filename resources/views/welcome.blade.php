9c185db328112ae791a197fbb89b628ce1c500a8
Bumpyy <fsghassan2429d@gmail.com>
2025-10-28 08:09:19 +0700

    initial frontpage rework

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,29 @@
 <x-layouts.app :gradient='true'>
-    <div class="container">
+    <div class="container my-12">
 
-        <div class="flex justify-center">
-            <img class="max-w-80" src="{{ asset('img/cedea-kfood.png') }}" alt="">
+        <div class="mb-12 flex justify-center">
+            <img class="clamp-[max-w,160px,280px]" src="{{ asset('img/cedea-kfood.png') }}" alt="Cedea & K-Food Logo">
         </div>
 
-        <div class="flex justify-center">
-            <img src="{{ asset('img/placeholder.png') }}" alt="">
+        <div class="flex flex-wrap">
+            <div class="flex flex-col items-center justify-start gap-4">
+                <div class="w-3/5">
+                    <img src="{{ asset('img/eomuk-bar-text.png') }}" alt="snack ala korea Eomuk Bar">
+                </div>
+                <div class="w-11/12">
+                    <img src="{{ asset('img/enak-bar-bar.png') }}" alt="Enaknya Bar Bar">
+                </div>
+            </div>
+
+            <div class="-ml-12 -mt-24 basis-3/5">
+                <div class="group flex w-full">
+                    <img class="ml-20 w-2/5 -rotate-3 drop-shadow-2xl transition-[rotate] duration-[600ms] ease-in-out group-hover:-rotate-1"
+                        src="{{ asset('img/eomuk-cheese-4.png') }}" alt="">
+                    <img class="-ml-[5.3rem] mt-16 w-2/5 rotate-6 drop-shadow-2xl transition-[rotate] duration-[600ms] ease-in-out group-hover:rotate-3"
+                        src="{{ asset('img/eomuk-chili-4.png') }}" alt="">
+                </div>
+                {{-- <img src="{{ asset('img/eomuk-combine.png') }}" alt="Eomuk Combine Image"> --}}
+            </div>
         </div>
 
         {{-- <x-home.prize />
